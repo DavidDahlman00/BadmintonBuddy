ada1027578d971eb8a5ef2c591a3ef18c6e81d86
David Dahlman <daviddahlman82@gmail.com>
2022-05-17 11:48:51 +0200

    fix hide password

--- app/src/main/java/duiban/badmintonbuddy/ui/login/LoginFragment.kt
+++ app/src/main/java/duiban/badmintonbuddy/ui/login/LoginFragment.kt
@@ -4,10 +4,13 @@ import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import duiban.badmintonbuddy.R
 import duiban.badmintonbuddy.databinding.FragmentLoginBinding
 import duiban.badmintonbuddy.ui.main.MainActivity
@@ -15,7 +18,9 @@ import duiban.badmintonbuddy.ui.main.MainActivity
 class LoginFragment : Fragment() {
 
     private var loginFragmentLoginBinding:FragmentLoginBinding? = null
-
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private var showPassword = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +28,21 @@ class LoginFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
         val binding = FragmentLoginBinding.bind(view)
         loginFragmentLoginBinding = binding
+
+        email = binding.logintextUseremail
+        password = binding.editTextTextPassword
+        password.requestFocus()
+        binding.imagelogineye.setOnClickListener {
+            if (showPassword){
+                binding.imagelogineye.setImageResource(R.drawable.ic_eye)
+                password.transformationMethod = PasswordTransformationMethod.getInstance()
+            }else{
+                binding.imagelogineye.setImageResource(R.drawable.ic_visibility_off)
+                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+
+            }
+            showPassword = !showPassword
+        }
 
         binding.loginButton.setOnClickListener {
             gotoMainscreen(this.requireContext())
--- app/src/main/java/duiban/badmintonbuddy/ui/login/RegisterFragment.kt
+++ app/src/main/java/duiban/badmintonbuddy/ui/login/RegisterFragment.kt
@@ -4,10 +4,13 @@ import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import duiban.badmintonbuddy.R
 import duiban.badmintonbuddy.databinding.FragmentRegisterBinding
 import duiban.badmintonbuddy.ui.main.MainActivity
@@ -16,7 +19,10 @@ import duiban.badmintonbuddy.ui.main.MainActivity
 class RegisterFragment() : Fragment() {
 
     private var registerBinding : FragmentRegisterBinding?= null
-
+    private lateinit var email: EditText
+    private lateinit var userName: EditText
+    private lateinit var password: EditText
+    private var showPassword = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +30,23 @@ class RegisterFragment() : Fragment() {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
         val binding = FragmentRegisterBinding.bind(view)
         registerBinding = binding
+
+        email = binding.registertextEmail
+        password = binding.registereditTextTextPassword
+        userName = binding.registertextUsername
+        password.requestFocus()
+
+        binding.imageeye.setOnClickListener {
+            if (showPassword){
+                binding.imageeye.setImageResource(R.drawable.ic_eye)
+                password.transformationMethod = PasswordTransformationMethod.getInstance()
+            }else{
+                binding.imageeye.setImageResource(R.drawable.ic_visibility_off)
+                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+
+            }
+            showPassword = !showPassword
+        }
         binding.registerButton.setOnClickListener {
             gotoMainscreen(this.requireContext())
         }

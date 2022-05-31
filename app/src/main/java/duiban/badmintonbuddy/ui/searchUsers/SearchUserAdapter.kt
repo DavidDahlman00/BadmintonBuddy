9580c748454d659306ac230fd77fc8284dec20ad
David Dahlman <daviddahlman82@gmail.com>
2022-05-31 17:49:35 +0200

    shows image in user search

--- app/src/main/java/duiban/badmintonbuddy/ui/searchUsers/SearchUserAdapter.kt
+++ app/src/main/java/duiban/badmintonbuddy/ui/searchUsers/SearchUserAdapter.kt
@@ -1,10 +1,13 @@
 package duiban.badmintonbuddy.ui.searchUsers
 
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import duiban.badmintonbuddy.R
 import duiban.badmintonbuddy.models.UserObject
 
@@ -21,6 +24,14 @@ class SearchUserAdapter : RecyclerView.Adapter<SearchUserAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: SearchUserAdapter.ViewHolder, position: Int) {
         holder.itemName.text = UserObject.userSearchList[position].name
         holder.itemEmail.text = UserObject.userSearchList[position].email
+        if (UserObject.userSearchList[position].profileImage == ""){
+            holder.itemImage.setImageResource(R.drawable.ic_person)
+        }else{
+            Picasso.
+            get().
+            load(UserObject.userSearchList[position].profileImage).
+            into(holder.itemImage)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -30,5 +41,6 @@ class SearchUserAdapter : RecyclerView.Adapter<SearchUserAdapter.ViewHolder>() {
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         var itemName: TextView = itemView.findViewById(R.id.search_user_name_text)
         var itemEmail: TextView = itemView.findViewById(R.id.search_user_email_text)
+        var itemImage: ImageView = itemView.findViewById(R.id.search_item_image)
     }
 }

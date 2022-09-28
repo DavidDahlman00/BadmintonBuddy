5442a45194b18991dc99f2896ae6705872a1077e
daviddahlman00 <davivdahlman82@gmail.com>
2022-09-28 16:08:45 +0200

    add rounded images

--- app/src/main/java/duiban/badmintonbuddy/ui/login/LoginFragment.kt
+++ app/src/main/java/duiban/badmintonbuddy/ui/login/LoginFragment.kt
@@ -3,16 +3,16 @@ package duiban.badmintonbuddy.ui.login
 import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -23,6 +23,7 @@ import duiban.badmintonbuddy.models.User
 import duiban.badmintonbuddy.models.UserObject
 import duiban.badmintonbuddy.ui.main.MainActivity
 
+
 class LoginFragment : Fragment() {
 
     private var loginFragmentLoginBinding: FragmentLoginBinding? = null
@@ -31,6 +32,7 @@ class LoginFragment : Fragment() {
     private lateinit var email: EditText
     private lateinit var password: EditText
     private var showPassword = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
